d2116b77978a183fd78c0f92bee31fe8fb302b60
David <david.pradomejuto@iesmiguelherrero.com>
2023-12-14 20:12:56 +0100

    update

---
+++ Tema4 Objetos/Hoja3-objetosPredefinidos/ejercicio7/fichero.js
@@ -0,0 +1,21 @@
+/* 
+Escribe una expresión regular que permita validar URL de sitios web, cuyos
+requisitos son que empiece por https://, que termine en .es y en medio no haya
+ninguna “ñ”.
+*/
+
+
+function escapeHTML(text){
+var replacements = //;
+
+    console.log(text.replace(replacements));
+}
+
+validarUrl("https://drivegoogle.es");
+
+validarUrl("https://drivegoogle.com");
+
+
+validarUrl("https://driveñgoogle.es");
+
+validarUrl("https://drive google.es");
